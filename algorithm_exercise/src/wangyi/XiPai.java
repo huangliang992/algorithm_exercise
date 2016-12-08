5568acbaeb8e0668b997ac4456603bc4caf3adc2
hliang <1512460987@qq.com>
2016-12-08 13:15:00 +0800

    洗牌

---
+++ algorithm_exercise/src/wangyi/XiPai.java
@@ -0,0 +1,40 @@
+package wangyi;
+
+import java.util.Scanner;
+
+public class XiPai {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int k=in.nextInt();
+		for(int i=0;i<k;i++){
+			int n=in.nextInt()*2;
+			int a []=new int [n];
+			int m=in.nextInt();
+			for(int j=0;j<n;j++){
+				a[j]=in.nextInt();
+			}
+			for(int h=0;h<m;h++){
+				a=xipai(a);
+			}
+			for(int j=0;j<n;j++){
+				if(j<n-1){
+				System.out.print(a[j]+" ");
+				}else{
+					System.out.print(a[j]);
+				}
+			}
+			System.out.println();
+		}
+	}
+	public static int [] xipai(int a[]){
+		int r[]=new int [a.length];
+		for(int i=0;i<a.length;i++){
+			if(i%2==0){
+				r[i]=a[i/2];
+			}else{
+				r[i]=a[(a.length+i-1)/2];
+			}
+		}
+		return r;
+	}
+}

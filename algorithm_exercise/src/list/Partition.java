9fb5b0fc1af074c6f8b5b1aed1d9c738d06e623d
hliang <1512460987@qq.com>
2017-01-15 14:05:53 +0800

    21

--- algorithm_exercise/src/list/Partition.java
+++ algorithm_exercise/src/list/Partition.java
@@ -1,5 +1,10 @@
 package list;
-
+/**
+ * 编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前
+给定一个链表的头指针 ListNode* pHead，请返回重新排列后的链表的头指针。注意：分割以后保持原来的数据顺序不变。
+ * @author hl
+ *
+ */
 public class Partition {
 	public ListNode partition(ListNode pHead, int x) {
         // write code here

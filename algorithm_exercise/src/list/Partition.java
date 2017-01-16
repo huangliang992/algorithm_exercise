04c97eef5d7b79e8deb5db36ea7614a25f417fb7
hliang <1512460987@qq.com>
2017-01-16 15:21:15 +0800

    partition

--- algorithm_exercise/src/list/Partition.java
+++ algorithm_exercise/src/list/Partition.java
@@ -1,4 +1,8 @@
 package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前
 给定一个链表的头指针 ListNode* pHead，请返回重新排列后的链表的头指针。注意：分割以后保持原来的数据顺序不变。
@@ -8,26 +12,57 @@ package list;
 public class Partition {
 	public ListNode partition(ListNode pHead, int x) {
         // write code here
-		ListNode head=null;
-		ListNode mid=null;
+		ListNode i=pHead;
 		int k=0;
-		ListNode tag1=pHead;
-		ListNode tag2=pHead;
-		while(tag1!=null){
+		while(i!=null){
 			k++;
-			if(k==x){
-				mid=tag1;
-				break;
-			}
-			tag1=tag1.next;
+			i=i.next;
 		}
-		while(tag2!=null){
-			if(tag2.val<mid.val){
-				head=tag2;
-				
-				
+		i=pHead;
+		int a[]=new int[k];
+		int j=0;
+		while(i!=null){
+			a[j]=i.val;
+			j++;
+			i=i.next;
+		}
+		List<Integer> l1=new ArrayList<Integer>();
+		List<Integer> l2=new ArrayList<Integer>();
+		for(int h=0;h<k;h++){
+			if(a[h]<=x){
+				l1.add(a[h]);
+			}else{
+				l2.add(a[h]);
 			}
 		}
-		return head;
+		for(int m=0;m<l1.size();m++){
+			a[m]=l1.get(m);
+		}
+		for(int m=0;m<l2.size();m++){
+			a[l1.size()+m]=l2.get(m);
+		}
+		i=pHead;
+		int n=0;
+		while(i!=null){
+			i.val=a[n];
+			i=i.next;
+			n++;
+		}
+		return pHead;
     }
+	
+	public static void main(String args[]){
+		Partition p=new Partition();
+		ListNode l=new ListNode(3);
+		l.next=new ListNode(4);
+		l.next.next=new ListNode(1);
+		l.next.next.next=new ListNode(5);
+		l.next.next.next.next=new ListNode(2);
+		l.next.next.next.next.next=new ListNode(7);
+		ListNode ln=p.partition(l, 3);
+		while(ln!=null){
+			System.out.println(ln.val);
+			ln=ln.next;
+		}
+	}
 }

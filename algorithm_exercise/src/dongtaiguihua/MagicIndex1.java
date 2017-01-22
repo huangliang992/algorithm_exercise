ddfa65f65be7c817d5161c2d268d6e742fffe61b
hliang <1512460987@qq.com>
2017-01-22 13:22:39 +0800

    22

---
+++ algorithm_exercise/src/dongtaiguihua/MagicIndex1.java
@@ -0,0 +1,14 @@
+package dongtaiguihua;
+
+public class MagicIndex1 {
+	public boolean findMagicIndex(int[] A, int n) {
+        // write code here
+		boolean f=false;
+		for(int i=0;i<n;i++){
+			if(A[i]==i){
+				return true;
+			}
+		}
+		return f;
+    }
+}

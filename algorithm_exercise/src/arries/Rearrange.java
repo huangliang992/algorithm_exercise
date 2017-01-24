d52053f8beafc24c89cde28894636efc1b73b04a
hliang <1512460987@qq.com>
2017-01-24 19:22:46 +0800

    22

--- algorithm_exercise/src/arries/Rearrange.java
+++ algorithm_exercise/src/arries/Rearrange.java
@@ -1,5 +1,43 @@
 package arries;
 
 public class Rearrange {
-
+	public int[] findSegment(int[] A, int n) {
+        // write code here
+		int k1=0;
+		int k2=0;
+		for(int i=0;i<n;i++){
+			int temp=A[i];
+			for(int j=i+1;j<n;j++){
+				if(A[j]<A[i]){
+					temp=A[j];
+					k1=i;
+					break;
+				}
+			}
+			if(temp<A[i]){
+				break;
+			}
+		}
+		for(int i=n-1;i>=0;i--){
+			int temp=A[i];
+			for(int j=i-1;j>=0;j--){
+				if(A[i]<A[j]){
+					temp=A[j];
+					k2=i;
+					break;
+				}
+			}
+			if(temp>A[i]){
+				break;
+			}
+		}
+		int result[]={k1,k2};
+		return result;
+    }
+	public static void main(String args[]){
+		Rearrange r=new Rearrange();
+		int A[]={1,4,6,5,11,10};
+		int a[]=r.findSegment(A, 6);
+		System.out.print(a[0]+" "+a[1]);
+	}
 }
